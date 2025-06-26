7751832d54e194d058b7bb0e91daf58d4fcbd8c8
Samuel Stephenson <sstephenson10469@gmail.com>
2025-06-26 11:15:35 -0400

    GetCart implemnted

--- easyshop.api/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ easyshop.api/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,16 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import java.util.List;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    void addItemToCart(int productId, int userId);
+    void updateItemQuantity(int userId, int productId, int quantity);
+    void deleteAllItem(int productId);
 }
