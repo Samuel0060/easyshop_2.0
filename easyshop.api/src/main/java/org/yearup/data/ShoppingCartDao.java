0cde43893bb985db96043aa8aa8c421ce2d572e2
DoulaD <douladoumbia@gmail.com>
2025-07-18 12:07:36 -0400

    shopping cart dao and controller

--- easyshop.api/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ easyshop.api/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,16 +1,12 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
-import org.yearup.models.ShoppingCartItem;
-
-import java.util.List;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
-
-    void addItemToCart(int productId, int userId);
-    void updateItemQuantity(int userId, int productId, int quantity);
-    void deleteAllItem(int productId);
+    ShoppingCart addProduct(int userId, int productId);
+    ShoppingCart update(int userId, int productId, int quantity);
+    void clear(int userId);
 }
